c59a4748fbaa7dbc0176ffa87af5fc988551ca0f
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-10 01:20:35 +0200

    :truck: update: images path

--- src/data/medias.ts
+++ src/data/medias.ts
@@ -1,73 +1,73 @@
 const photos = [
     {
       name: "image00001",
-      url: "/src/assets/photos/image00001.jpeg",
+      url: "../src/assets/photos/image00001.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00002",
-      url: "/src/assets/photos/image00002.jpeg",
+      url: "../src/assets/photos/image00002.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00003",
-      url: "/src/assets/photos/image00003.jpeg",
+      url: "./src/assets/photos/image00003.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00004",
-      url: "/src/assets/photos/image00004.jpeg",
+      url: "./src/assets/photos/image00004.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00005",
-      url: "/src/assets/photos/image00005.jpeg",
+      url: "./src/assets/photos/image00005.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00006",
-      url: "./src/assets/photos/image00006.jpeg",
+      url: "../src/assets/photos/image00006.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00007",
-      url: "/src/assets/photos/image00007.jpeg",
+      url: "./src/assets/photos/image00007.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00008",
-      url: "/src/assets/photos/image00008.jpeg",
+      url: "./src/assets/photos/image00008.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00009",
-      url: "/src/assets/photos/image00009.jpeg",
+      url: "./src/assets/photos/image00009.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00010",
-      url: "/src/assets/photos/image00010.jpeg",
+      url: "./src/assets/photos/image00010.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00011",
-      url: "/src/assets/photos/image00011.jpeg",
+      url: "./src/assets/photos/image00011.jpeg",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00012",
-      url: "/src/assets/photos/image00012.jpeg",
+      url: "./src/assets/photos/image00012.jpeg",
       authorName: "",
       authorLink: ""
     }
