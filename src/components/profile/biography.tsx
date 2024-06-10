01a639e97b3bf9d62ff2afda931006f8eef2d652
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-10 04:21:04 +0200

    :lipstick: update: correct biography

--- src/components/profile/biography.tsx
+++ src/components/profile/biography.tsx
@@ -5,7 +5,7 @@ function Biography() {
         Born in April 1989 in Lyon to a Martiniquais mother and a Congolese father, Sandra Parfait pursued studies in interpretation while simultaneously taking drama classes.
       </p>
       <p>
-        She became an actress in 2013 after earning a bachelor's degree in LEA (English, Spanish, Japanese) and began working in theater in plays such as "A Season in the Congo" by Aimé Césaire, "The Vagina Monologues" by Eve Ensler, and "Some Spice in the Eyes" by Simon Grangeat.
+        She became an actress in 2013 after earning a bachelor's degree in LEA (English, Spanish, Japanese) and began working in theater in plays such as "Une Saison au Congo" by Aimé Césaire, "The Vagina Monologues" by Eve Ensler, and "Du Piment dans Les Yeux" by Simon Grangeat.
       </p>
       <p>
         She obtained her first television roles in productions by Didier Le Pêcheur, Jérôme Cornuau, Slimane-Baptiste Berhoun, Andrew Fleming, and Simon Astier, among others, and in cinema with Nicolas Pariser and Antonin Peretjatko.
