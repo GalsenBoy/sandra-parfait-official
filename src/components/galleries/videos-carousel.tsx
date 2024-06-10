2b84f792825cf21238a3bd9eb0052dc099509136
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-10 02:34:54 +0200

    :fix::rocket:

--- src/components/galleries/videos-carousel.tsx
+++ src/components/galleries/videos-carousel.tsx
@@ -17,6 +17,7 @@ function VideosCarousel() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </CarouselItem>
+        
         <CarouselItem>
           <iframe
             className='w-full aspect-video'
