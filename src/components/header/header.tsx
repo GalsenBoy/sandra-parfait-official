e77450508a906b8b39843311204b535011dc882b
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-11 05:45:33 +0200

    :mag: update: remove stuntwoman from the titles and meta-tags

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ function Header() {
       <header className="bg-header bg-no-repeat bg-top bg-cover h-svh grid grid-rows-subgrid row-span-3 justify-center p-10">
         <div id="title" className="row-start-2 self-center flex flex-col gap-2">
           <h1 className="text-5xl font-black">Sandra Parfait</h1>
-          <p id="subtitles" className="text-2xl">· Actress - Stuntwoman ·</p>
+          <p id="subtitles" className="text-2xl">· Actress ·</p>
         </div>
 
         <nav className="row-start-3 self-end">
