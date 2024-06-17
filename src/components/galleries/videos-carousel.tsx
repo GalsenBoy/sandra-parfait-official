0d076cf2edb61dc661bf692ce892a6327ded7033
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-17 16:29:19 +0200

    :card_file_box: update: add a YouTube videos of one Conann 35 scenes

--- src/components/galleries/videos-carousel.tsx
+++ src/components/galleries/videos-carousel.tsx
@@ -17,7 +17,13 @@ function VideosCarousel() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </CarouselItem>
-        
+        <CarouselItem>
+          <iframe className='w-full aspect-video'
+            src="https://www.youtube.com/embed/qQd080up3o4?si=882fnGA570KvN1F1"
+            title="YouTube video player" frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </CarouselItem>
         <CarouselItem>
           <iframe
             className='w-full aspect-video'
