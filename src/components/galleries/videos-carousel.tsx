6baf9921c9696293f1dfeec98581c63bc4c6a841
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-06-17 16:36:28 +0200

    :card_file_box: update: add Conann trailer YTVideo

--- src/components/galleries/videos-carousel.tsx
+++ src/components/galleries/videos-carousel.tsx
@@ -25,6 +25,13 @@ function VideosCarousel() {
             referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </CarouselItem>
         <CarouselItem>
+          <iframe className='w-full aspect-video'
+            src="https://www.youtube.com/embed/beysI2CopYM?si=s7I5tzFDuhnKlkBM&amp;start=38"
+            title="YouTube video player" frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </CarouselItem>
+        <CarouselItem>
           <iframe
             className='w-full aspect-video'
             src="https://www.youtube.com/embed/gFCX5iScflw?si=T7w8Cl91NOw8DQA0"
