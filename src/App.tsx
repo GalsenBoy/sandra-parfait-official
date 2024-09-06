c180c4eba6d3532da22c5d72ab4d9ddcd3929599
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-09-06 16:23:29 +0200

    Reapply ":zap: update: improve performance by adding suspense on sections and suppressing it on single video"
    This reverts commit e0efbeff8cb45fc35fad4b63f49beec87b381b55.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import Footer from './components/footer/footer'
 import GoToUp from './components/footer/go-to-top-button'
 import Header from './components/header/header'
 import Profile from './components/profile/profile'
 import { ThemeProvider } from './components/theme-provider'
-const News = lazy(()=> import('./components/news/news'))
-const Galleries = lazy(()=> import('./components/galleries/galleries'))
+const News = lazy(() => import('./components/news/news'))
+const Galleries = lazy(() => import('./components/galleries/galleries'))
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Header />
-      <News />
+      <Suspense>
+        <News />
+      </Suspense>
       <Profile />
-      <Galleries />
+      <Suspense>
+        <Galleries />
+      </Suspense>
       <Footer />
       <GoToUp />
     </ThemeProvider>
