333ec302451263a8ba6b6b997049d4e18b6f3237
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-09-06 15:52:18 +0200

    :zap: update: improve performance with lazy loading section containing videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { lazy } from 'react'
 import './App.css'
 import Footer from './components/footer/footer'
 import GoToUp from './components/footer/go-to-top-button'
-import Galleries from './components/galleries/galleries'
 import Header from './components/header/header'
-import News from './components/news/news'
 import Profile from './components/profile/profile'
 import { ThemeProvider } from './components/theme-provider'
+const News = lazy(()=> import('./components/news/news'))
+const Galleries = lazy(()=> import('./components/galleries/galleries'))
 
 function App() {
   return (
