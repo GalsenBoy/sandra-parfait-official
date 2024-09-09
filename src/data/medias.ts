910f42133d7a7373bb3af317f921018ea56dc4de
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-09-09 20:08:34 +0200

    :zap: update: replace jpeg with webp photos to improve performance

--- src/data/medias.ts
+++ src/data/medias.ts
@@ -1,73 +1,73 @@
 const photos = [
     {
       name: "image00001",
-      url: "assets/photos/image00001.jpeg",
+      url: "assets/photos/image00001.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00002",
-      url: "assets/photos/image00002.jpeg",
+      url: "assets/photos/image00002.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00003",
-      url: "assets/photos/image00003.jpeg",
+      url: "assets/photos/image00003.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00004",
-      url: "assets/photos/image00004.jpeg",
+      url: "assets/photos/image00004.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00005",
-      url: "assets/photos/image00005.jpeg",
+      url: "assets/photos/image00005.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00006",
-      url: "assets/photos/image00006.jpeg",
+      url: "assets/photos/image00006.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00007",
-      url: "assets/photos/image00007.jpeg",
+      url: "assets/photos/image00007.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00008",
-      url: "assets/photos/image00008.jpeg",
+      url: "assets/photos/image00008.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00009",
-      url: "assets/photos/image00009.jpeg",
+      url: "assets/photos/image00009.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00010",
-      url: "assets/photos/image00010.jpeg",
+      url: "assets/photos/image00010.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00011",
-      url: "assets/photos/image00011.jpeg",
+      url: "assets/photos/image00011.webp",
       authorName: "",
       authorLink: ""
     },
     {
       name: "image00012",
-      url: "assets/photos/image00012.jpeg",
+      url: "assets/photos/image00012.webp",
       authorName: "",
       authorLink: ""
     }
