2bb319471953ad15c61f430467283adc9463f8e4
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-09-10 16:37:46 +0200

    :wheelchair: update: add aria-label to go to up button

--- src/components/footer/go-to-top-button.tsx
+++ src/components/footer/go-to-top-button.tsx
@@ -2,7 +2,11 @@ import { CircleArrowUp } from "lucide-react"
 
 function GoToUp() {
   return (
-    <button className="sticky bottom-4 float-right right-4" onClick={() => window.scroll({ top: 0 })}>
+    <button 
+    aria-label="Go to top"
+    className="sticky bottom-4 float-right right-4" 
+    onClick={() => window.scroll({ top: 0 })}
+    >
       <CircleArrowUp size={54} />
     </button>
   )
